2f2f9843ead6b5d43e21a4d979b9fbefa5d9c296
MaggieWesolowska <maggiewes07@gmail.com>
2023-04-08 18:22:40 +0200

    added js files

---
+++ js-api-crud-sync-await/js/store.js
@@ -0,0 +1,91 @@
+// Store MODULE
+
+// note: all functions are IMPURE (because they manipulate Local Storage)
+
+// ########################################
+// Private functions (not-exported)
+// ########################################
+
+// UPDATE
+const setPosts = (newPosts) => {
+  try {
+    const json = JSON.stringify(newPosts);
+    localStorage.setItem("posts", json);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// ########################################
+// Public functions (exported)
+// ########################################
+
+// READ
+const getPosts = () => {
+  try {
+    return JSON.parse(localStorage.getItem("posts")) || [];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// CREATE
+const initPosts = (posts) => {
+  const postsWithIsEdited = posts.map((p) => ({
+    ...p,
+    isEdited: false,
+  }));
+
+  setPosts(postsWithIsEdited);
+};
+
+const addPost = (newPost) => {
+  const oldPosts = getPosts();
+  const newPosts = [...oldPosts, newPost];
+  setPosts(newPosts);
+};
+
+// UPDATE
+const editPost = ({ id, title, body }) => {
+  const oldPosts = getPosts();
+  const newPosts = oldPosts.map((p) =>
+    p.id === id
+      ? {
+          ...p,
+          title,
+          body,
+          isEdited: false,
+        }
+      : p
+  );
+
+  setPosts(newPosts);
+};
+
+const togglePostIsEdited = (postId) => {
+  const oldPosts = getPosts();
+  const newPosts = oldPosts.map((p) =>
+    p.id === postId ? { ...p, isEdited: !p.isEdited } : p
+  );
+
+  setPosts(newPosts);
+};
+
+// DELETE
+const deletePost = (postId) => {
+  const oldPosts = getPosts();
+  const newPosts = oldPosts.filter((p) => p.id !== postId);
+
+  setPosts(newPosts);
+};
+
+const Store = {
+  getPosts,
+  initPosts,
+  addPost,
+  editPost,
+  togglePostIsEdited,
+  deletePost,
+};
+
+export default Store;
---
+++ js-api-crud-sync-await/js/view.js
@@ -0,0 +1,92 @@
+import ApiPosts from "./api/posts";
+import Dom from "./utils/dom";
+import Store from "./store";
+
+const postsDOM = Dom.qs("#posts");
+
+// DOM functions
+const viewUsers = () => {
+  const posts = Store.getPosts();
+
+  const templateIsEdited = ({ id, title, body }) => `
+	<form class="editPostForm" data-id="${id}">
+		<h3>Edit Post</h3>
+		<input type="text" name="title" placeholder="title" value="${title}" />
+		<textarea name="body" cols="30" placeholder="body" rows="5">${body}</textarea>
+		<button class="btn--create">Confirm</button>
+  </form>
+	<button class="btn--toggle-edit" data-id="${id}" data-toggle-edit>Cancel</button>
+	`;
+  const templateIsNotEdited = (id) => `
+	<button class="btn--toggle-edit" data-id="${id}" data-toggle-edit>Edit</button>
+	`;
+
+  postsDOM.innerHTML = posts
+    .map(
+      (p) => `
+      <li class="post">
+      <h2 class="post__title">(id:${p.id}) ${p.title}</h2>
+      <p class="post__body">${p.body}</p>
+			${
+        p.isEdited
+          ? templateIsEdited(p)
+          : templateIsNotEdited(p.id)
+      }
+      <button class="btn--delete" data-id="${
+        p.id
+      }" data-delete>Delete</button>
+    </li>`
+    )
+    .join("");
+
+  // Is Edited Btns
+  const toggleEditBtns = Dom.qsa(
+    "button[data-id][data-toggle-edit]"
+  );
+  toggleEditBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const postId = Number(e.target.dataset.id);
+
+      // Data update
+      Store.togglePostIsEdited(postId);
+
+      // View update
+      viewUsers();
+
+      // Confirm btns / Form
+      const editPostForms = Dom.qsa("form.editPostForm");
+      editPostForms.forEach((form) => {
+        form.addEventListener("submit", async (e) => {
+          e.preventDefault();
+
+          const postId = Number(e.target.dataset.id);
+
+          const { title, body } = e.currentTarget.elements;
+
+          const postData = {
+            title: title.value,
+            body: body.value,
+          };
+
+          ApiPosts.editPost(postId, postData);
+        });
+      });
+    });
+  });
+
+  // Delete Btns
+  const deleteBtns = Dom.qsa(
+    "button[data-id][data-delete]"
+  );
+  deleteBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const postId = Number(e.target.dataset.id);
+
+      ApiPosts.deletePost(postId);
+    });
+  });
+};
+
+const View = { viewUsers };
+
+export default View;
